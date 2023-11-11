230185e0ee50aa6c6951993adeb9217d0462ae50
Sabr01 <150509847+Sabr01@users.noreply.github.com>
2023-11-11 15:32:24 +0600

    laba1-2-3
    test

--- Лаба 1/src/main/kotlin/Main.kt
+++ Лаба 1/src/main/kotlin/Main.kt
@@ -1,32 +1,58 @@
-// Лаба 1 задача
+/*
+//2 задание 1 лабы
 fun main() {
-    // тут я присвоил значения
-    val a = 10
 
-       val b = 5
+    print("Введите число: ")
 
-    // (сложение, вычитание, умножение, деление)
-    val sum = a + b
-
-           val difference = a - b
-
-        val product = a * b
-    val quotient = a.toDouble() / b.toDouble() // деление, чтобы результат был с плавающей точкой
-
-    // тут результаты
-    println("Сумма: $sum")
-       println("Разность: $difference")
-     println("Произведение: $product")
-    println("Частное: $quotient")
-
-    // сравниваем
-    println("a равно b: ${a == b}")
-     println("a не равно b: ${a != b}")
-    println("a больше b: ${a > b}")
-    println("a меньше b: ${a < b}")
-    println("a больше или равно b: ${a >= b}")
-       println("a меньше или равно b: ${a <= b}")
+    // преобразуем введенное число
+    val userInput = readLine()?.toIntOrNull()
 
 
+    if (userInput != null) {
+
+        if (userInput % 2 == 0) {
+            println("Введенное число $userInput четное.")
+        } else {
+            println("Введенное число $userInput нечетное.")
+        }
+    } else {
+        println("Ошибка Некорректный ввод.")
+    }
 }
+*/
+/*
+//3 задача 1 лабы А) и Б)
+
+fun main() {
+    val number = 5
+
+    println("Таблица умножения для $number:")
+
+    for (i in 1..10) {
+        val result = number * i
+        println("$number * $i = $result")
+    }
+}
+*/
+fun main() {
+    var userInput: Int?
+
+    do {
+        print("Введите число (введите 0 для выхода): ")
+        userInput = readLine()?.toIntOrNull()
+
+        if (userInput != null && userInput != 0) {
+            val square = userInput * userInput
+            println("Квадрат числа $userInput: $square")
+        } else if (userInput != 0) {
+            println("Некорректный ввод. Пожалуйста, введите целое число.")
+        }
+    } while (userInput != 0)
+
+    println("Программа завершена.")
+}
+
+
+
+
 
